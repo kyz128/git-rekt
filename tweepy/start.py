1f6ddb191b05e6f6e933f022bf98c28b0d520c6f
Marisa Roldan <34078523+Muhrisa@users.noreply.github.com>
2019-07-11 13:20:54 -0400

    prints out tweets now

--- tweepy/start.py
+++ tweepy/start.py
@@ -1,5 +1,7 @@
 import tweepy
-
+from datetime import datetime, date, time, timedelta
+from collections import Counter
+import sys
 # We use OAuth 2 authentication in this app for read-only access.
 
 # For OAuth 2 and OAuth 1 authentication; needed for read-only access
@@ -9,11 +11,35 @@ auth = tweepy.AppAuthHandler("Lfdjsisd57ew2Bc0nvEXeyBXL", "THNlus5JSo3mSJvli1y0X
 # auth = tweepy.OAuthHandler("Lfdjsisd57ew2Bc0nvEXeyBXL", "THNlus5JSo3mSJvli1y0Xgwe8adIjwlAKWHHuFhaagUWFywE05")
 # auth.set_access_token(access_token, access_secret)
 
-api = tweepy.API(auth)
+auth_api = tweepy.API(auth)
+
+
+account_list = ["KingJames"]
+
+
+if len(account_list) > 0:
+  for target in account_list:
+    print("Getting data for " + target)
+    item = auth_api.get_user(target)
+    #print(item)
+    print("name: " + item.name)
+    print("screen_name: " + item.screen_name)
+
+    tweet_count = 0
+    end_date = datetime.utcnow() - timedelta(days=60)
+    for status in tweepy.Cursor(auth_api.user_timeline, id=target).items():
+      print(status.text)
+      tweet_count += 1
+      if status.created_at < end_date:
+        break
+    print(tweet_count)
+
 
 # Example OAuth 2 authentication function
-for tweet in tweepy.Cursor(api.search, q='tweepy').items(10):
-    print(tweet.text)
+#for tweet in tweepy.Cursor(api.search, q='tweepy').items(10):
+#    print(tweet.text)
+
+
 
 # Example OAuth 1 authentication function
 # public_tweets = api.home_timeline()
