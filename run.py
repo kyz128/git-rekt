4f93354cdba67c4cdcf07884bc544f189c1aee2b
Marisa Roldan <34078523+Muhrisa@users.noreply.github.com>
2019-07-11 11:57:20 -0400

    Update run.py

--- run.py
+++ run.py
@@ -16,4 +16,28 @@ with open(join(dirname(__file__), './profile.json')) as profile_json:
         consumption_preferences=True,
         raw_scores=True
     ).get_result()
-print(json.dumps(profile, indent=2))
+#print(json.dumps(profile, indent=2))
+
+#n_scores = profile["needs"]
+scores_dict = dict()
+
+def json_dict(profile_str):
+    scores_dict[profile_str] = []
+    for v in profile[profile_str]:
+        temp_dict = dict()
+        temp_dict["name"] = v["name"]
+        temp_dict["raw_score"] = v["raw_score"]
+        scores_dict[profile_str].append(temp_dict)
+
+def get_scores(profile_str):
+        scores_dict[profile_str] = dict()
+        for v in profile[profile_str]:
+            scores_dict[profile_str][v["name"]] = v["raw_score"]
+
+
+get_scores("personality")
+get_scores("needs")
+get_scores("values")
+
+
+print(scores_dict["values"]["Conservation"])
